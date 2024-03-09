8bcac09ab8435c0093e7b813b23f5728302f3e8d
hesam <97361994+hesy87@users.noreply.github.com>
2024-03-09 13:11:05 +0100

    file name changed

--- src/store/dataSlice.js
+++ src/store/dataSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
-import { ticketsData } from '../mocks/tickets'
+import { ticketsData } from '../Mocks/tickets'
 
 // initial data comes from mock data and attach to redux
 const initialState = {
